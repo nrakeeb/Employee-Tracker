d12ffa66cf2034c7cb56730ad30d203416db7dd6
Nouha Rakeeb <Nouha_rakeeb@hotmail.co.uk>
2021-11-01 20:47:55 +0000

    added schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,9 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE departments(
+CREATE TABLE departments (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(30) NOT NULL,
+name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
@@ -13,20 +13,21 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
 department_id INT,
-FOREIGN KEY (department_id),
+FOREIGN KEY (department_id)
 REFERENCES departments(id)
 ON DELETE SET NULL 
 );
 
 CREATE TABLE employees (
- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
  first_name VARCHAR(30) NOT NULL,
  last_name VARCHAR(30) NOT NULL,
  role_id INT,
- manager_id INT -- need to search null if employee has no manager--
- FOREIGN KEY (role_id),
- REFERENCES roles(id),
- FOREIGN KEY (manager_id),
- REFERENCES employees(id),
+ manager_id INT DEFAULT NULL,
+ FOREIGN KEY (role_id)
+ REFERENCES roles(id)
+ ON DELETE SET NULL, 
+ FOREIGN KEY (manager_id)
+ REFERENCES employees(id)
  ON DELETE SET NULL 
 );
