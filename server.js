16089e0059f6f02d0527b35a9553f7f6346cd7a5
nouha rakeeb <nouha_rakeeb@hotmail.co.uk>
2021-11-02 14:56:03 +0000

    chore: commented delete query out and inserted import statement
    Authors: NR

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
 
+import {viewDepartments} from './queries';
+
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -18,9 +21,50 @@ const db = mysql.createConnection(
   
 );
 
-db.query('SELECT * FROM employees', function (err, results) {
-  console.log(results);
-});
+function getDepartments() {
+  let result= viewDepartments()
+  console.log(result)
+}
+
+// db.query('SELECT * FROM employees', function (err, results) {
+//   console.log(results);
+// });
+
+
+// // delete department
+// db.query(`DELETE FROM departments WHERE id = ${department}`, function (err, result) {
+//  if (err) {
+//    console.log(err)
+//  }
+//   else if(result) {
+//   console.log("department deleted")
+//  }
+ 
+// });
+
+// // delete roles 
+// db.query(`DELETE FROM roles WHERE id = ${role}`, function (err, result) {
+//   if (err) {
+//     console.log(error)
+//   }
+//    else if (result) {
+//    console.log("role deleted")
+//   }
+  
+//  });
+
+//  // delete employee 
+// db.query(`DELETE FROM employees WHERE id = ${employee}`, function (err, result) {
+//   if (err) {
+//     console.log(error)
+//   }
+//    else if (result) {
+//    console.log("employee deleted")
+//   }
+  
+//  });
+
+ 
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
