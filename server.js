825e738739caa18df9cff12a5e0186ff32b6a8de
Nouha Rakeeb <Nouha_rakeeb@hotmail.co.uk>
2021-11-03 23:58:21 +0000

    chore: added dotenv to hide db credentials

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
+require('dotenv').config()
 
 const dbQueries = require("./db/queries");
 
@@ -11,9 +12,9 @@ app.use(express.json());
 
 const db = mysql.createConnection(
   {
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'employee_db'
   },
   console.log(`Connected to the employee_db database.`)
