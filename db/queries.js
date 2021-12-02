d0cce7f87311a9a5b133ffb99b5ef68685319a12
nouha rakeeb <nouha_rakeeb@hotmail.co.uk>
2021-12-02 17:13:05 +0000

    refactor: add department , role and Employee in queries.js
    Authors: NR

--- db/queries.js
+++ db/queries.js
@@ -5,34 +5,120 @@ class Queries {
         this.db = db;
     }
 
-    viewDepartments(db) {
-        db.query('SELECT * FROM departments', function (err, results) {
+    // ----------------------------------------------- VIEW -----------------------------------------------------------------//
+
+    viewDepartments() {
+        this.db.query('SELECT * FROM departments', function (err, results) {
             console.log(`\n`);
             console.table(results);
         });
     }
 
-    viewEmployees(db) {
-        db.query('SELECT * FROM employees', function (err, results) {
+    viewEmployees() {
+        this.db.query('SELECT * FROM employees', function (err, results) {
             console.log(`\n`);
             console.table(results);
         });
     }
 
-    viewRoles(db) {
-        db.query('SELECT * FROM roles', function (err, results) {
+    viewRoles() {
+        this.db.query('SELECT * FROM roles', function (err, results) {
             console.log(`\n`);
             console.table(results);
         });
     }
 
-    viewEmployeesByDepartment(db, departmentId) {
-        console.log("HERE");
-        db.query(`SELECT * FROM employees JOIN roles ON employees.role_id = roles.id WHERE roles.department_id = ${departmentId}`, function (err, results) {
+    viewEmployeesByDepartment(departmentId) {
+        this.db.query(`SELECT * FROM employees JOIN roles ON employees.role_id = roles.id WHERE roles.department_id = ${departmentId}`, function (err, results) {
             console.log(`\n`);
             console.table(results);
         })
-    };
+    }
+
+    viewByManager(managerId){
+        this.db.query(`SELECT employees.id, first_name, last_name, roles.title AS title, roles.salary AS salary, manager_id FROM employees JOIN roles ON employees.role_id = roles.id WHERE manager_id = ${managerId};`, function (err, result) {
+            console.log(`\n`);
+            console.table(results);
+        })
+    }
+
+    // -------------------------------------------------- ADD -----------------------------------------------------------------//
+
+    addDepartment(name) {
+        this.db.query(`INSERT INTO departments(name) VALUES ("${name}")`, function (err, results) {
+            if (err) {
+                throw err;
+            } else {
+                console.log("department added into the database")
+            }
+        }
+        )
+    }
+
+    newEmployee(firstName, lastName, employeeRoleId, employeeManagerId) {
+        this.db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("${firstName}", "${lastName}", "${employeeRoleId}", "${employeeManagerId}");`, function (err, result) {
+            if (err) {
+                console.error(err)
+            } else {
+                console.log(`Database updated with new employee.`)
+                console.log(`"${firstName}", "${lastName}" added successfully.\n`)
+                console.log(`\n`);
+            };
+        });
+    }
+
+    newRole(title, salary, department){
+        this.db.query(`INSERT INTO roles (title, salary, department_id) VALUES ("${title}", ${salary}, ${department});`, function (err, result) {
+            if (err) {
+                console.error(err)
+            } else {
+                console.log(`Database updated with new role.`)
+                console.log(`"${title}" added successfully.\n`)
+                console.log(`\n`);
+            };
+        });
+
+    }
+    
+
+    // ------------------------------------------------------- UPDATE ---------------------------------------------------------------//
+
+
+    updateEmployee(){
+    this.db.query()
+
+    }
+
+
+    // ------------------------------------------------------ MAPPING TABLES--------------------------------------------------------//
+
+    async rolesChoice() {
+        const [data] = await this.db.promise()
+        .query('SELECT id, title FROM roles;');
+        
+    const roles = data.map(result => { return {name: result.title, value: result.id} })
+    return roles;
+    
 }
+
+    async employeesChoice() {
+        const [data] = await this.db.promise()
+        .query('SELECT id, first_name, last_name FROM employees;');
+        
+    const managers = data.map(result => { return {name: `${result.first_name} ${result.last_name}`, value: result.id} })
+    return managers;
+    
+}
+
+async departmentChoice() {
+    const [data] = await this.db.promise()
+    .query('SELECT * FROM departments;');
+    
+const department = data.map(result => { return {name: result.name, value: result.id} })
+return department;
+
+}
+}
+ 
 
 module.exports = Queries;
